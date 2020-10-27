e11b04e42c93a373c46cb20e0531d85eaf683957
karthik-mohan <68692486+karthik-mohan@users.noreply.github.com>
2020-10-27 23:40:51 +0530

    Add files via upload

---
+++ house.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, render_template
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+model = joblib.load("house.pkl")
+
+@app.route("/")
+def hello():
+    return render_template('house.html')
+
+@app.route('/predict',methods = ['POST'])
+def result():
+    if request.method == 'POST':
+        UC = request.form["Under Construction"]
+        uc = int(UC)
+        print(uc)
+        RERA = request.form["RERA"]
+        RERA = int(RERA)
+        print(RERA)
+        BHK = request.form["BHK"]
+        BHK = int(BHK)
+        print(uc)
+        SQFT = request.form["SQFT"]
+        SQFT = float(SQFT)
+        print(SQFT)      
+        list1=[[uc,RERA,BHK,SQFT]]  
+        print(list1)
+        price = np.abs(model.predict(list1))
+        return render_template('house.html',prediction_text="Price in Lakhs={}".format(price))
+
+if __name__ == "__main__":
+    app.run()
